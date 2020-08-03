71c0c4f75bf02a3824e2951c92e13f7ad3c8e752
Bartek <WonderfulLifeVision@gmail.com>
2020-08-03 19:23:06 +0200

    navbar animations appear only if viewport is NOT scrolled

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -12,39 +12,43 @@ export class WelcomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-      //scroll block
-      // $('body').addClass('stop-scrolling');
+    //scroll block
+    // $('body').addClass('stop-scrolling');
 
 
-      $(".nav-link").hover(function(){
+
+    $(".nav-link").hover(function () {
+      if (!$("#navbar").hasClass("scrolled")) {
         $("#firstWelcomeLine #mainWelcome").addClass("BiggerIcon");
-        if($(this).text() === "Work"){
-            $("#firstWelcomeLine #mainWelcome").html("<i class='fas fa-window-maximize'></i>");
+        if ($(this).text() === "Work") {
+          $("#firstWelcomeLine #mainWelcome").html("<i class='fas fa-window-maximize'></i>");
         }
-        else if($(this).text() === "Courses"){
-                $("#firstWelcomeLine #mainWelcome").html("<i class='fas fa-book'></i>");
+        else if ($(this).text() === "Courses") {
+          $("#firstWelcomeLine #mainWelcome").html("<i class='fas fa-book'></i>");
         }
-        else if($(this).text() === "Github"){
-                $("#firstWelcomeLine #mainWelcome").html("<i class='fab fa-github-alt'></i>");
+        else if ($(this).text() === "Github") {
+          $("#firstWelcomeLine #mainWelcome").html("<i class='fab fa-github-alt'></i>");
         } else {
-            $("#firstWelcomeLine #mainWelcome").html("<i class='far fa-surprise'></i>");
+          $("#firstWelcomeLine #mainWelcome").html("<i class='far fa-surprise'></i>");
         }
         $("#secondWelcomeLine #mainWelcome").hide();
-      }, function(){
-        $("#firstWelcomeLine #mainWelcome").removeClass("BiggerIcon");
-        $("#firstWelcomeLine #mainWelcome").html("Hey I am Bartek");
-        $("#secondWelcomeLine #mainWelcome").show();
-      });
+      };
+    }, function () {
+      $("#firstWelcomeLine #mainWelcome").removeClass("BiggerIcon");
+      $("#firstWelcomeLine #mainWelcome").html("Hey I am Bartek");
+      $("#secondWelcomeLine #mainWelcome").show();
+    });
 
 
-      //Click "Hi..."
-      $("[id=mainWelcome]").click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#secondSection").offset().top
-        }, 600);
 
-        $('body').removeClass('stop-scrolling');
-      });
+    //Click "Hi..."
+    $("[id=mainWelcome]").click(function () {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#secondSection").offset().top
+      }, 600);
+
+      $('body').removeClass('stop-scrolling');
+    });
 
   }
 
