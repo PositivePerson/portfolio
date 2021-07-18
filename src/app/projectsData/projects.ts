a177c3e1ad6a0c09f98c0602714b998970f6a5d7
Bartosz <WonderfulLifeVision@gmail.com>
2021-07-18 09:18:43 +0200

    Projects data updated

--- src/app/projectsData/projects.ts
+++ src/app/projectsData/projects.ts
@@ -25,19 +25,6 @@ export const PROJECTS: Project[] = [
     link: "https://fun-riding.herokuapp.com/",
     description: ["YelpCamp - app that allows to create accounts", "Post images with description, put comments, edit etc.", "Thanks to databases it saves everything", "Passwords are protected so remains stored with safety. "],
     tools:
-      // {
-      //   Javascript: true,
-      //   jQuery: true,
-      //   HTML5: true,
-      //   CSS3: true,
-      //   Bootstrap: true,
-      //   Semantic: true,
-      //   Angular: false,
-      //   React: false,
-      //   MongoDB: true,
-      //   Hawler: false,
-      //   PaperScript: false
-      // }
       [
         "Javascript",
         "HTML5",
@@ -48,65 +35,69 @@ export const PROJECTS: Project[] = [
         "Express"
       ]
   },
+  // {
+  //   id: 3,
+  //   name: "TodoPage",
+  //   img: "TodoPage.png",
+  //   link: "https://positiveperson.github.io/To-Do-List/",
+  //   description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."],
+  //   tools:
+  //     [
+  //       "Javascript",
+  //       "jQuery",
+  //       "HTML5",
+  //       "CSS3"
+  //     ]
+  // },
+  // {
+  //   id: 3,
+  //   name: "MuseumOfCandy",
+  //   img: "CandyPage.png",
+  //   link: "https://positiveperson.github.io/MuseumOfCandy/",
+  //   description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."],
+  //   tools:
+  //     [
+  //       "HTML5",
+  //       "CSS3",
+  //       "Bootstrap"
+  //     ]
+  // },
   {
     id: 3,
-    name: "TodoPage",
-    img: "TodoPage.png",
-    link: "https://positiveperson.github.io/To-Do-List/",
-    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."],
-    tools:
-      // {
-      //   Javascript: true,
-      //   jQuery: true,
-      //   HTML5: true,
-      //   CSS3: true,
-      //   Bootstrap: true,
-      //   Semantic: false,
-      //   Angular: false,
-      //   React: false,
-      //   MongoDB: false,
-      //   Hawler: false,
-      //   PaperScript: false
-      // }
-      [
-        "Javascript",
-        "jQuery",
-        "HTML5",
-        "CSS3"
-      ]
-  },
-  {
-    id: 4,
-    name: "MuseumOfCandy",
-    img: "CandyPage.png",
-    link: "https://positiveperson.github.io/MuseumOfCandy/",
-    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."],
-    tools:
-      //     {
-      //       Javascript: false,
-      //       jQuery: false,
-      //       HTML5: true,
-      //       CSS3: true,
-      //       Bootstrap: true,
-      //       Semantic: false,
-      //       Angular: false,
-      //       React: false,
-      //       MongoDB: false,
-      //       Hawler: false,
-      //       PaperScript: false
-      //     }
-      [
-        "HTML5",
-        "CSS3",
-        "Bootstrap"
-      ]
-  },
-  {
-    id: 5,
     name: "CircleSite",
     img: "circlesv2.png",
     link: "https://positiveperson.github.io/CircleSite/",
     description: ["Turn on sound.", " Click different keys and watch what happen", "Project created with Javascript in 98%", "Inspiration: patatap."],
+    tools:
+      [
+        "Javascript",
+        "Howler",
+        "Paper",
+        "HTML5",
+        "CSS3"
+      ]
+   },
+  {
+    id: 4,
+    name: "Kittypedia",
+    img: "kittypedia.png",
+    link: "https://kittypedia.vercel.app/",
+    description: ["Kittypedia", " Many kitties will appear there", "They render randomly from database of thousands of cats", "All created using react context for API in Next.js structure"],
+    tools:
+      [
+        "Javascript",
+        "Howler",
+        "Paper",
+        "HTML5",
+        "CSS3"
+      ]
+   },
+  {
+    id: 5,
+    name: "Turn the light on",
+    img: "turn_the_light_on.png",
+    link: "https://turn-the-lights-on.vercel.app/",
+    description: ["React game", "Find the switch hidden in darkness and turn it on!", "As soon as you start the game timer will start marking milliseconds", "After that - slider above let you manage amount of light on the page.", "Good luck!"],
     tools:
       //     {
       //       Javascript: true,
